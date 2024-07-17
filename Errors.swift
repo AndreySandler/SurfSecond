9a9b03d33d9296af52399a7ff0f048040acbbb71
Andrey Sandler <mrdreem13@gmail.com>
2024-07-17 19:26:13 +0300

    Вроде какой-то базовый шаблон сделал

---
+++ Errors.swift
@@ -0,0 +1,8 @@
+//
+//  Errors.swift
+//  SurfSecond
+//
+//  Created by Andrey Sandler on 17.07.2024.
+//
+
+import Foundation
---
+++ ErrorsView.swift
@@ -0,0 +1,8 @@
+//
+//  ErrorsView.swift
+//  SurfSecond
+//
+//  Created by Andrey Sandler on 17.07.2024.
+//
+
+import Foundation
